fe0a44e9889b2e690235e8530c614d30fb2ddae5
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-06-23 13:49:34 +0200

    Buscador Funcionando

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Buscador = () => {
+export const Buscador = ({ listadoState, setListadoState }) => {
+  const [busqueda, setBusqueda] = useState("");
+  const [noEncontrado, setNoEncontrado] = useState(false);
+
+  const buscarPeli = (e) => {
+    // Crear estado y actualizarlo
+    setBusqueda(e.target.value);
+    // Filtrar para buscar coincidencias
+    let pelisEncontradas = listadoState.filter((peli) => {
+      return peli.titulo.toLowerCase().includes(busqueda.toLowerCase());
+    });
+    if (busqueda.length <= 1 || pelisEncontradas <= 0) {
+      pelisEncontradas = JSON.parse(localStorage.getItem("pelis"));
+      setNoEncontrado(true);
+    } else {
+      setNoEncontrado(false);
+    }
+
+    // Actualizar eestado del listado principal con lo que logro filtrar
+    setListadoState(pelisEncontradas);
+  };
+
   return (
     <div className="search">
-      <h3 className="title">Buscador</h3>
+      <h3 className="title">Buscador: {busqueda}</h3>
+      {noEncontrado === false ||
+        (busqueda.length > 1 && (
+          <span className="no-encontrado">
+            No se ha encontrado ninguna coincidencia
+          </span>
+        ))}
+
       <form>
-        <input type="text" id="search_field" />
+        <input
+          type="text"
+          id="search_field"
+          name="busqueda"
+          autoComplete="off"
+          value={busqueda}
+          onChange={buscarPeli}
+        />
         <button id="search">Buscar</button>
       </form>
     </div>
