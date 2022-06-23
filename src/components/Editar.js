11f956437bf96b51e50f2b4587f3f9dc8ea02c08
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-06-23 12:44:16 +0200

    Estructura basica formulario edicion

--- src/components/Editar.js
+++ src/components/Editar.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const Editar = () => {
-  return <h1>FORMULARIO</h1>;
+  const tituloComponente = "Editar pelicula";
+
+  return (
+    <div className="edit_form">
+      <h3 className="title">{tituloComponente}</h3>
+      <from>
+        <input
+          type="text"
+          name="titulo"
+          className="titulo_editado"
+          defaultValue="Titulo orginal de la pelicula"
+        />
+        <textarea
+          name="descripcion"
+          defaultValue="descripcion original"
+          className="descripcion_editada"
+        />
+        <input type="submit" className="editar" value="Actualizar" />
+      </from>
+    </div>
+  );
 };
